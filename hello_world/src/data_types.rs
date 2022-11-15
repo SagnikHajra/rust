b58380f385416b42402854fcd83f53561071d15b
shajra <hajra.sagnik@gmail.com>
2022-11-15 00:15:04 -0600

    first commit

---
+++ hello_world/src/data_types.rs
@@ -0,0 +1,4 @@
+pub fn run(){
+    let x: f64 = 10.83274682462846287346;
+    println!("The num is {}", x);
+}
---
+++ hello_world/src/main.rs
@@ -0,0 +1,7 @@
+pub mod arithmetic;
+pub mod data_types;
+
+fn main() {
+    data_types::run();
+    arithmetic::run();
+}
